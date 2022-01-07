f707da534272f20b8919c4ae9f0f61e3002f332b
matt6514 <matt6514@live.unc.edu>
2022-01-07 10:32:29 -0500

    new classes

---
+++ season_class.py
@@ -0,0 +1,12 @@
+from classes import Team, Game
+from datetime import datetime
+from classplus import Tournament
+
+class Season:
+    def __init__(self,division):
+        self.division = division
+        self.games = games
+        self.teams = teams
+        self.tournaments = tournaments
+
+    
